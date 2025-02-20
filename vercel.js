6ec42cc387ecc647e6f947134fe64069033b619e
itdevexe <vihaan@growthspree.in>
2025-02-20 17:00:03 +0530

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,1 +1,20 @@
+{
+  "rewrites": [
+
+    {"source": "/fix-a-bug", "destination": "https://fix-a-bug.vercel.app/"},
+    {"source": "/fix-a-bug/:match*", "destination": "https://fix-a-bug.vercel.app/:match*"},
+
+    
+    {"source": "/security-testing", "destination": "https://fix-a-bug-security-testing.vercel.app/"},
+    {"source": "/security-testing/:match*", "destination": "https://fix-a-bug-security-testing.vercel.app/:match*"},
+
+    
+    {"source": "/mobile-testing", "destination": "https://fix-a-bug-mobile-testing.vercel.app/"},
+    {"source": "/mobile-testing/:match*", "destination": "https://fix-a-bug-mobile-testing.vercel.app/:match*"},
+
+    
+    {"source": "/software-testing", "destination": "https://fix-a-bug-software-testing.vercel.app/"},
+    {"source": "/software-testing/:match*", "destination": "https://fix-a-bug-software-testing.vercel.app/:match*"}
+    ]
+}
 
